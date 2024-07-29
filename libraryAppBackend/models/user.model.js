803aef12d1d36ef85426d0594d8f9b65e97a1af3
jeevan-2005 <jeevanchoudhary2005@gmail.com>
2024-07-29 11:45:01 +0530

    created UserModel for both students and admin...

---
+++ libraryAppBackend/models/user.model.js
@@ -0,0 +1,13 @@
+const mongoose = require("mongoose");
+
+const userSchema = mongoose.Schema({
+  userName: { type: String, required: true },
+  email: { type: String, required: true, unique: true },
+  password: { type: String, required: true },
+  gender: { type: String, required: true },
+  role: { type: String, enum: ["STUDENT", "ADMIN"], required: true },
+});
+
+const UserModel = mongoose.model("user", userSchema);
+
+module.exports = UserModel;
