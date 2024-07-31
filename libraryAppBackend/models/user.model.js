b01d1732ccc3f22a965ca0cb7c7c14f7f7f3f98b
jeevan-2005 <jeevanchoudhary2005@gmail.com>
2024-07-31 12:17:02 +0530

    added a feild of userVerification...

--- libraryAppBackend/models/user.model.js
+++ libraryAppBackend/models/user.model.js
@@ -1,11 +1,12 @@
 const mongoose = require("mongoose");
 
-const userSchema = mongoose.Schema({
+const userSchema = new mongoose.Schema({
   userName: { type: String, required: true },
   email: { type: String, required: true, unique: true },
   password: { type: String, required: true },
   gender: { type: String, required: true },
   role: { type: String, enum: ["STUDENT", "ADMIN"], required: true },
+  verified: {type: Boolean, default: false}
 });
 
 const UserModel = mongoose.model("user", userSchema);
