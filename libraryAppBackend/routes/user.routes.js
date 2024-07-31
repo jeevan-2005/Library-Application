a156df1dad4eace3ab321a3fd7faf9c5a3c5b834
jeevan-2005 <jeevanchoudhary2005@gmail.com>
2024-07-31 12:16:24 +0530

    created verifyEmail route...

--- libraryAppBackend/routes/user.routes.js
+++ libraryAppBackend/routes/user.routes.js
@@ -5,6 +5,9 @@ const userRouter = express.Router();
 const jwt = require("jsonwebtoken");
 const blacklistModel = require("../models/blackListToken.model");
 require("dotenv").config();
+const emailVerificationToken = require("../models/emailToken.model");
+const sendEmail = require("../utils/sendEmail");
+const crypto = require("crypto");
 
 const tokenSecretKey = process.env.TOKEN_SECRET_KEY;
 const refershTokenSecretKey = process.env.REFRESH_TOKEN_SECRET_KEY;
@@ -24,15 +27,24 @@ userRouter.post("/register", async (req, res) => {
             .status(500)
             .send({ msg: "Error occured while hashing the password" });
 
-        const newUser = new UserModel({
+        const newUser = await new UserModel({
           userName,
           email,
           password: hash,
           gender,
           role,
+        }).save();
+
+        const token = await new emailVerificationToken({
+          userId: newUser._id,
+          token: crypto.randomBytes(32).toString("hex"),
+        }).save();
+        const url = `${process.env.BASE_URL}user/${newUser._id}/verify/${token.token}`;
+        await sendEmail(newUser.email, "Verify Email", url);
+
+        res.status(200).send({
+          msg: "An email has been sent to your email address, Please verify",
         });
-        await newUser.save();
-        res.status(200).send({ msg: "User registration successful." });
       });
     }
   } catch (error) {
@@ -47,11 +59,28 @@ userRouter.post("/login", async (req, res) => {
     const user = await UserModel.findOne({ email });
     // console.log(user.password)
     if (user) {
-      bcrypt.compare(password, user.password, (err, result) => {
+      bcrypt.compare(password, user.password, async (err, result) => {
         if (err)
           res
             .status(500)
             .send({ msg: "error occured while comparing passwords" });
+
+        if (!user.verified) {
+          let token = await emailVerificationToken.findOne({
+            userId: user._id,
+          });
+          if (!token) {
+            token = await new emailVerificationToken({
+              userId: newUser._id,
+              token: crypto.randomBytes(32).toString("hex"),
+            }).save();
+            const url = `${process.env.BASE_URL}user/${newUser._id}/verify/${token.token}`;
+            await sendEmail(newUser.email, "Verify Email", url);
+          }
+
+          res.status(400).send({msg: "An Email has been sent to your email address, Please verify",});
+        }
+
         if (result && role == user.role) {
           const accessToken = jwt.sign({ ...user }, tokenSecretKey, {
             expiresIn: "1h",
@@ -59,6 +88,7 @@ userRouter.post("/login", async (req, res) => {
           const refreshToken = jwt.sign({ ...user }, refershTokenSecretKey, {
             expiresIn: "1d",
           });
+
           res.status(200).send({
             msg: "Login Successful",
             token: accessToken,
@@ -85,6 +115,32 @@ userRouter.get("/logout", async (req, res) => {
   let blacklistToken = new blacklistModel({ token });
   await blacklistToken.save();
   res.status(200).send({ msg: "user logged out successfully." });
+});
+
+userRouter.get("/:id/verify/:token", async (req, res) => {
+  try {
+    const { id, token } = req.params;
+
+    const user = await UserModel.findOne({ _id: id });
+    if (!user) return res.status(400).send({ msg: "Invalid Link" });
+
+    // Check if user is already verified
+    if (user.verified) return res.status(400).send({ msg: "User already verified" });
+
+    const verifyToken = await emailVerificationToken.findOne({
+      userId: user._id,
+      token,
+    });
+    if (!verifyToken) return res.status(400).send({ msg: "Invalid Link" });
+
+    await UserModel.updateOne({ _id: user._id }, { $set: { verified: true } });
+    await verifyToken.deleteOne();
+
+    res.status(200).send({ msg: "Email verified successfully." });
+  } catch (error) {
+    console.log(error);
+    res.status(500).send({ msg: "Internal server error." });
+  }
 });
 
 module.exports = userRouter;
