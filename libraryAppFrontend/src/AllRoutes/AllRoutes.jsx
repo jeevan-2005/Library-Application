754a33c68a714eda4995dd9f6a6b83f232a1dbfe
jeevan-2005 <jeevanchoudhary2005@gmail.com>
2024-08-01 11:16:49 +0530

    email verify route added...

--- libraryAppFrontend/src/AllRoutes/AllRoutes.jsx
+++ libraryAppFrontend/src/AllRoutes/AllRoutes.jsx
@@ -1,20 +1,36 @@
-import {Navigate, Route, Routes} from 'react-router-dom'
-import Login from '../Pages/Login/Login'
-import Register from '../Pages/Register/Register'
-import Admin from '../Users/Admin/Admin'
-import Student from '../Users/Student/Student'
-import ProtectedComponent from '../components/protectedComponent/ProtectedComponent'
+import { Navigate, Route, Routes } from "react-router-dom";
+import Login from "../Pages/Login/Login";
+import Register from "../Pages/Register/Register";
+import Admin from "../Users/Admin/Admin";
+import Student from "../Users/Student/Student";
+import ProtectedComponent from "../components/protectedComponent/ProtectedComponent";
+import VerifyEmail from "../Pages/VerifyEmail/VerifyEmail";
 
 const AllRoutes = () => {
   return (
     <Routes>
-        <Route path="/login" element={<Login />} />
-        <Route path='/admin' element={<ProtectedComponent role="ADMIN" ><Admin /></ProtectedComponent>} />
-        <Route path='/student' element={<ProtectedComponent role="STUDENT" ><Student /></ProtectedComponent>} />
-        <Route path='/register' element={<Register />} />
-        <Route path='*' element={<Navigate to="/login" />} />
+      <Route path="/login" element={<Login />} />
+      <Route
+        path="/admin"
+        element={
+          <ProtectedComponent role="ADMIN">
+            <Admin />
+          </ProtectedComponent>
+        }
+      />
+      <Route
+        path="/student"
+        element={
+          <ProtectedComponent role="STUDENT">
+            <Student />
+          </ProtectedComponent>
+        }
+      />
+      <Route path="/register" element={<Register />} />
+      <Route path="/user/:id/verify/:token" element={<VerifyEmail />} />
+      <Route path="*" element={<Navigate to="/login" />} />
     </Routes>
-  )
-}
+  );
+};
 
-export default AllRoutes
+export default AllRoutes;
