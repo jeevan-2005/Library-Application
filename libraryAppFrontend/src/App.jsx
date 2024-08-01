bbe16af81d1e838b1b62a9e36ad43e726409d068
jeevan-2005 <jeevanchoudhary2005@gmail.com>
2024-08-01 11:17:34 +0530

    allRoutes added...

--- libraryAppFrontend/src/App.jsx
+++ libraryAppFrontend/src/App.jsx
@@ -1,10 +1,6 @@
-import { useContext } from "react";
 import AllRoutes from "./AllRoutes/AllRoutes";
-import { AuthContext } from "./Contexts/AuthContext";
 
 function App() {
-  const {authState} = useContext(AuthContext)
-  console.log(authState)
   return (
     <>
       <AllRoutes/>
